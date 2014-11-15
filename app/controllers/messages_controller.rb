046cd4378748159539fb4ef9640324ad1337c7b6
Rodrigo Ferreira <rodrigo.ferreira@locaweb.com.br>
2014-11-15 12:50:03 -0200

    Message#index show only parent message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,7 @@ class MessagesController < ApplicationController
   # GET /messages
   # GET /messages.json
   def index
-    @messages = Message.all
+    @messages = Message.where(parent: nil)
   end
 
   # GET /messages/1
